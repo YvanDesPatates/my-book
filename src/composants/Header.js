a27224f837a96051b9f3ede06e30a3b3012f7092
Yvan Roux <yvis99@gmail.com>
2025-02-12 17:01:26 +0100

    Header generalization with data

--- src/composants/Header.js
+++ src/composants/Header.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import '../ressources/css/header.css';
 import phone from '../ressources/images/phone.png';
-import { Link } from 'react-router-dom';
+import {getDomaineCompetencylist} from "../ressources/domaine_competency_info";
+import {Link} from 'react-router-dom';
 
-const domaineKey_forExemple = "IA"
 const get_domaine_url = (domaineKey) => {
     return '/domaine_de_competence/'.concat(domaineKey)
 }
@@ -32,25 +32,31 @@ export default function Header() {
     return (
         <div className='header-div-container'>
             {windowWidth > 768 ? (
-                <div className='header-div-phone'>
-                    <img className='header-img-phone' src={phone} alt='icone de téléphone' />
-                    <a className='telto' href='tel:0662116016'>
-                        <p className='header-p-phone'>06 62 11 60 16</p>
-                    </a>
-                    <div className='header-svg-mail'>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280 320-200v-80L480-520 160-720v80l320 200Z" /></svg>
-                    </div>
-                    <a className='mailto' href='mailto:yvanroux99@gmail.com'>
-                        <p className='header-p-phone'>yvanroux99@gmail.com</p>
-                    </a>
-                </div>)
+                    <div className='header-div-phone'>
+                        <img className='header-img-phone' src={phone} alt='icone de téléphone'/>
+                        <a className='telto' href='tel:0662116016'>
+                            <p className='header-p-phone'>06 62 11 60 16</p>
+                        </a>
+                        <div className='header-svg-mail'>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+                                <path
+                                    d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280 320-200v-80L480-520 160-720v80l320 200Z"/>
+                            </svg>
+                        </div>
+                        <a className='mailto' href='mailto:yvanroux99@gmail.com'>
+                            <p className='header-p-phone'>yvanroux99@gmail.com</p>
+                        </a>
+                    </div>)
                 : null
             }
             <div className='header-div-menu'>
                 <Link to='/' className='header-p-menu'>HOME</Link>
-                <Link to={get_domaine_url(domaineKey_forExemple)} className='header-p-menu quandMeme'>Un domaine</Link>
+                {getDomaineCompetencylist().map((domaine, index) => (
+                    <Link key={index} to={get_domaine_url(domaine.key)} className='header-p-menu'> {domaine.key} </Link>
+                ))}
                 {!isMenuOpen && (
-                    <svg className='svg-header-menu' onClick={toggleMenu} xmlns="http://www.w3.org/2000/svg" width="40" height="30" viewBox="0 0 40 30">
+                    <svg className='svg-header-menu' onClick={toggleMenu} xmlns="http://www.w3.org/2000/svg" width="40"
+                         height="30" viewBox="0 0 40 30">
                         <g fill="currentColor">
                             <rect width="40" height="5"></rect>
                             <rect y="12.5" width="40" height="5"></rect>
@@ -61,21 +67,29 @@ export default function Header() {
             </div>
             {isMenuOpen && (
                 <div className="menu-burger">
-                        <svg className='svg-close-menu' onClick={toggleMenu} xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
-                            <g fill="none" stroke="black" stroke-width="5">
-                                <line x1="10" y1="10" x2="30" y2="30" />
-                                <line x1="30" y1="10" x2="10" y2="30" />
-                            </g>
-                        </svg>
+                    <svg className='svg-close-menu' onClick={toggleMenu} xmlns="http://www.w3.org/2000/svg" width="40"
+                         height="40" viewBox="0 0 40 40">
+                        <g fill="none" stroke="black" stroke-width="5">
+                            <line x1="10" y1="10" x2="30" y2="30"/>
+                            <line x1="30" y1="10" x2="10" y2="30"/>
+                        </g>
+                    </svg>
                     <div className='container-menu-link'>
                         <Link className='item-menu' to='/' onClick={toggleMenu}>HOME</Link>
                         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="1" viewBox="0 0 30 1">
-                            <rect width="30" height="1" fill="black" />
+                            <rect width="30" height="1" fill="black"/>
                         </svg>
-                        <Link className='item-menu' to={get_domaine_url(domaineKey_forExemple)} onClick={toggleMenu}>Un domaine</Link>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="1" viewBox="0 0 30 1">
-                            <rect width="30" height="1" fill="black" />
-                        </svg>
+
+                        {getDomaineCompetencylist().map((domaine, index) => (
+                            <div key={index} className={"item-menu-link"}>
+                                <Link className='item-menu' to={get_domaine_url(domaine.key)}
+                                      onClick={toggleMenu}> {domaine.key} </Link>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="1" viewBox="0 0 30 1">
+                                    <rect width="30" height="1" fill="black"/>
+                                </svg>
+                            </div>
+                        ))}
+
                     </div>
                     <div className='header-menu-bottom'>
                         <a className='telto' href='tel:0662116016'>
