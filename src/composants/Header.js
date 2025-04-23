a80cb7df9f2b94854812d2d98b06b394769e3acc
yvan <yvis99@gmail.com>
2025-04-23 18:10:59 +0200

    update Header with animation and navigation

--- src/composants/Header.js
+++ src/composants/Header.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import {getDomaineCompetencylist} from "../ressources/domaine_competency_info";
 import {Link} from 'react-router-dom';
+import {useHandleNavigate} from "./UtilFunctions";
 
 const get_domaine_url = (domaineKey) => {
     return '/domaine_de_competence/'.concat(domaineKey)
@@ -8,18 +9,23 @@ const get_domaine_url = (domaineKey) => {
 
 export default function Header() {
     const [menuOpen, setMenuOpen] = useState(false);
+    const handleNavigate = useHandleNavigate();
 
     return (
         <div className='header-div-container flex flex-col'>
             <nav className="py-6 px-4 sm:px-6 lg:px-8">
                 <div className="max-w-7xl mx-auto flex justify-between items-center">
-                    <a href='#' className="text-2xl font-bold text-sky-700">Yvan Roux</a>
+                    <a
+                        className="text-2xl font-bold text-sky-700 simple-hover-animation"
+                        onClick={() => handleNavigate('/')}
+                        style={{ cursor: 'pointer' }}
+                    >Yvan Roux</a>
                     <div className="hidden md:flex space-x-8">
-                        <a href="tel:0662116016" className="flex items-center space-x-2 text-gray-600 font-medium hover:text-sky-700 transition">
+                        <a href="tel:0662116016" className="flex items-center space-x-2 text-gray-600 font-medium hover:text-sky-700 transition simple-hover-animation">
                             <i className="fas fa-phone-alt"></i>
                             <span>06 62 11 60 16</span>
                         </a>
-                        <a href="mailto:yvanroux99@gmail.com" className="flex items-center space-x-2 text-gray-600 hover:text-sky-700 transition">
+                        <a href="mailto:yvanroux99@gmail.com" className="flex items-center space-x-2 text-gray-600 hover:text-sky-700 transition simple-hover-animation">
                             <i className="fas fa-envelope"></i>
                             <span>yvanroux99@gmail.com</span>
                         </a>
