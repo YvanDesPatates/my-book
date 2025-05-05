6979e21570bce84de7652c7e9b33e1e4d31e2c3d
yvan <yvis99@gmail.com>
2025-05-05 11:19:43 +0200

    handle responsive with only one component

--- src/views/DomaineCompetency.js
+++ src/views/DomaineCompetency.js
@@ -1,8 +1,5 @@
-import {useMediaQuery} from 'react-responsive'
 import React from 'react';
 import {useParams} from 'react-router-dom';
-import DomaineCompetencyPhone from "../composants/DomaineCompetencyPhone";
-import DomaineCompetencyPc from "../composants/DomaineCompetencyPc";
 import {getDomaineCompetencyByKey} from "../ressources/domaine_competency_info";
 
 export default function DomaineCompetency() {
@@ -10,18 +7,8 @@ export default function DomaineCompetency() {
 
     const domaineCompetency = getDomaineCompetencyByKey(params.domaineKey)
 
-    const isPhone = useMediaQuery({query: '(max-width: 1000px)'})
     return (
         <div>
-            {isPhone ?
-                <DomaineCompetencyPhone
-                    domaineKey={domaineCompetency.key}
-                />
-                :
-                <DomaineCompetencyPc
-                    domaineKey={domaineCompetency.key}
-                />
-            }
         </div>
     )
 }
