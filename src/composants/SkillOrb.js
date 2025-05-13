f963088f3666a68a1950d51b1e86d8e1c0d84c27
yvan <yvis99@gmail.com>
2025-05-13 22:20:00 +0200

    skillorb: remove useless and bugfull tailwind classes

--- src/composants/SkillOrb.js
+++ src/composants/SkillOrb.js
@@ -1,13 +1,13 @@
+import '../ressources/css/skillOrb.css';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../ressources/css/skillOrb.css';
 
 export default function SkillOrb({ iconClass, gradientClass, colorClass, link, floatingClass }) {
     const navigate = useNavigate();
 
     return (
         <div
-            className={`skill-orb ${floatingClass || ''} ${gradientClass} ${colorClass} w-full h-full rounded-full flex items-center justify-center`}
+            className={`skill-orb ${floatingClass || ''} ${gradientClass} ${colorClass} rounded-full flex items-center justify-center`}
             onClick={() => navigate(link)}
             style={{ cursor: link ? 'pointer' : 'default' }}
         >
