ccd764f78b5a003ffa93910814e597ce9297832b
ROUX YVAN <yvan.roux@inrae.fr>
2025-06-03 16:14:25 +0200

    fill description infos

--- src/ressources/domaine_competency_info.js
+++ src/ressources/domaine_competency_info.js
@@ -1,19 +1,26 @@
 const domaine_competency = [
     {
         key:"Game-Developer",
+        description: "La création de jeux vidéo est un fil rouge depuis mes premiers pas dans le développement. Entre projets personnels, projets étudiants et game jams, j'ai pu explorer différents aspects du développement de jeux 2D, 3D et VR.",
+        tags: ["Unity", "C#", "VR", "3D", "2D", "game design", "architecture logicielle"],
         projects_keys: ["dogjammmer", "whatsinpic", "poorsausage"],
     },
     {
         key: "Web-Developer",
+        description: "Développeur web avec plus de <strong>3 ans d'expériences</strong>, j'ai une maitrise poussée des environnements REST et des frameworks front-end modernes. Bien qu'ayant une large préférence pour le backend, je suis un développeur fullstack polyvalent</br> </br>J'ai également développé de nombreux projets personnels et étudiants durant mes <strong>5 années d'études</strong>, ce qui m'as permis de travailler sur des technologies variées et de me spécialiser dans la création d'applications web performantes et maintenables.",
+        tags: ["API", "Java", "NodeJS", "Typescript", "React", "VueJS", "frameworks JS", "architecture logicielle"],
         projects_keys: ["personaljsframework", "opensilex", "whatsinpic", "dogjammmer", "poorsausage"],
     },
     {
-        key: "IA",
-        projects_keys: ["dogjammmer", "whatsinpic", "poorsausage"],
+        key: "Architecture-logiciel",
+        description: "L'architecture logicielle est un domaine qui me passionne particulièrement. J'ai eu l'occasion de travailler sur des projets complexes, notamment durant mes <strong>3 années en tant que développeur</strong> pour <a style='color: #7F00FF' href='http://www.opensilex.org/'>OpenSILEX</a>, où la conception et la structuration du code étaient essentielles pour garantir la maintenabilité et l'évolutivité des applications. J'ai également une bonne connaissance des principes SOLID et des design patterns.",
+        projects_keys: ["personaljsframework", "opensilex"],
     },
     {
-        key: "Architecture-logiciel",
-        projects_keys: ["personaljsframework", "opensilex"],
+        key: "IA",
+        description: "Mes 4èmes et 5èmes années d'études en <strong>spécialitées IA et Big Data</strong> m'ont permis d'explorer les domaines de l'intelligence artificielle. Entrainement de modèles, composition par couches, pipelines de données, et bien d'autres sujets ont été abordés durant ces deux années. J'ai également pu mettre en pratique ces connaissances dans le cadre de projets étudiants.",
+        tags: ["NLP", "LLM", "Python", "machine learning"],
+        projects_keys: ["dogjammmer", "whatsinpic", "poorsausage"],
     },
     {
         key: "Blender",
