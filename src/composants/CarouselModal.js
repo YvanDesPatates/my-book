98d1ecafa176e8b322084e045556fb0f85a53a56
ROUX YVAN <yvan.roux@inrae.fr>
2025-06-18 16:28:12 +0200

    carousel : improve button style and next/previous interaction zone

--- src/composants/CarouselModal.js
+++ src/composants/CarouselModal.js
@@ -23,8 +23,8 @@ export default function CarouselModal({images}) {
 
     return (
         <>
-            <button className="open-modal-button" onClick={openModal}>
-                Open Carousel
+            <button className="open-carousel-modal-button" onClick={openModal}>
+                See more images
             </button>
             {isOpen && (
                 <div className="carousel-modal-overlay" onClick={closeModal}>
@@ -33,14 +33,16 @@ export default function CarouselModal({images}) {
                             <button className="carousel-close-button" onClick={closeModal}>
                                 &times;
                             </button>
-                            <button className="carousel-arrow prev-arrow" onClick={handlePrev}>
-                                &#8249;
-                            </button>
+                            <div className="arrows">
+                                <button className="carousel-arrow prev-arrow" onClick={handlePrev}>
+                                    &#8249;
+                                </button>
+                                <button className="carousel-arrow next-arrow" onClick={handleNext}>
+                                    &#8250;
+                                </button>
+                            </div>
                             <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`}
                                  className="carousel-image"/>
-                            <button className="carousel-arrow next-arrow" onClick={handleNext}>
-                                &#8250;
-                            </button>
                         </div>
                         <div className="carousel-points">
                             {images.map((_, index) => (
